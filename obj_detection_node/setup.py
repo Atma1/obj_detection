e71986c75ef33b059848b45f18f4716a5971c6f2
Atma1 <atmasusanto08@gmail.com>
2024-05-30 17:28:48 +0800

    added path to new package

--- obj_detection_node/setup.py
+++ obj_detection_node/setup.py
@@ -6,13 +6,15 @@ package_name = 'obj_detection_node'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.0.2',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
+        (os.path.join('share', package_name+'/models'), glob('models/*.pt')),
+        (os.path.join('share', package_name+'/util'), glob('util/*.py')),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*')))
+        (os.path.join('share', package_name), glob('launch/*launch.[pxy][yma]*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -23,7 +25,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'detection_publisher = obj_detection_pub.publish_dectection:main'
+            'detection_publisher = obj_detection_node.publish_dectection:main',
+            'localization_publisher = obj_detection_node.publish_localization:main'
         ],
     },
 )
