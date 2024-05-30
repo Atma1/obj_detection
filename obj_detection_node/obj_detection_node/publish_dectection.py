3f0a57cb42e2930ff2cff77dd61451e374db1d25
Atma1 <atmasusanto08@gmail.com>
2024-05-30 17:29:34 +0800

    updated

--- obj_detection_node/obj_detection_node/publish_dectection.py
+++ obj_detection_node/obj_detection_node/publish_dectection.py
@@ -4,54 +4,62 @@ from ament_index_python.packages import get_package_share_directory
 from rclpy.node import Node
 from ultralytics import YOLO
 from sensor_msgs.msg import Image
-from obj_detection_interfaces.msg import Detection
-from vision_msgs.msg import Detection2DArray, ObjectHypothesis
+from obj_detection_interfaces.msg import Detection, DetectionArray
+from vision_msgs.msg import ObjectHypothesis
+from util.util import center_crop
 
 class DetectionPublisher(Node):
 
     def __init__(self):
         super().__init__("detection_node")
         self.declare_parameter("model_name", "yolov8n.pt")
+        self.declare_parameter("conf_threshold", 0.7)
         self.model_name = self.get_parameter("model_name").get_parameter_value().string_value
+        self.conf_threshold = self.get_parameter("conf_threshold").get_parameter_value().double_value
         self.path = get_package_share_directory("obj_detection_node")
-        self.publisher_ = self.create_publisher(Detection2DArray, "detection_node/detection", 10)
-        self.subscriber_ = self.create_subscription(Image, "detection_node/image", self.callback)
+        self.publisher_ = self.create_publisher(DetectionArray, "detection_node/detection", 10)
+        self.subscriber_ = self.create_subscription(Image, "detection_node/image", self.callback, 10)
         self.bridge = cv_bridge.CvBridge()
         self.model = YOLO(f"{self.path}/models/{self.model_name}")
+        self.get_logger().info('Initializing detection publisher')
 
-    def callback(self, image):
+    def callback(self, image: Image) -> None:
         cv_image = self.bridge.imgmsg_to_cv2(image, desired_encoding="bgr8")
-        results = self.infer(cv_image)
+        center_cropped = center_crop(cv_image, (640, 640))
+        results = self.infer(center_cropped)
         if results is not None:
             msg = self.create_message(results=results)
             self.publisher_.publish(msg=msg)
             self.get_logger().info(f"Published inference")
+        else:
+            self.get_logger().info("No detection.")
 
     def infer(self, image):
-        result = self.model.predict(image)
+        result = self.model.predict(image, conf=self.conf_threshold)
 
         return result
     
     def create_message(self, results):
-        detection_msg = Detection2DArray()
+        detection_array = DetectionArray()
         bounding_box = results[0].boxes.xyxy
         classes = results[0].boxes.cls
         confidence_score = results[0].boxes.conf
 
         for bbox, cls, conf in zip(bounding_box, classes, confidence_score):
             detection = Detection()
-            detection.top_leftmost_x = float(bbox[0])
-            detection.top_leftmost_y = float(bbox[1])
-            detection.bottom_rightmost_x = float(bbox[2])
-            detection.bottom_rightmost_y = float(bbox[3])
+            detection.top_leftmost_x = int(bbox[0])
+            detection.top_leftmost_y = int(bbox[1])
+            detection.bottom_rightmost_x = int(bbox[2])
+            detection.bottom_rightmost_y = int(bbox[3])
+
             hypothesis = ObjectHypothesis()
-            hypothesis.class_id = results[0].names.get(int(cls))
+            hypothesis.class_id = str(results[0].names.get(int(cls)))
             hypothesis.score = float(conf)
             detection.hypothesis = hypothesis
-            detection_msg.detections.append(detection)
+            detection_array.results.append(detection)
 
-        return detection_msg
-
+        print(detection_array)
+        return detection_array
 
 def main(args=None):
     rclpy.init(args=args)
---
+++ obj_detection_node/obj_detection_node/publish_localization.py
@@ -0,0 +1,52 @@
+import rclpy
+from rclpy.node import Node
+from obj_detection_interfaces.msg import DetectionArray, Localization, LocalizationArray
+from geometry_msgs.msg import Point
+from util.util import get_degree, get_distance, convert_to_cm
+
+class Localization(Node):
+    def __init__(self):
+        super().__init__('localization_node')
+        self.declare_parameter("img_height", value=640)
+        self.declare_parameter("img_width", value=640)
+        self.image_height =  self.get_parameter("img_height").get_parameter_value().double_value
+        self.image_width =  self.get_parameter("img_width").get_parameter_value().double_value
+        self.img_size = (self.image_width, self.image_height)
+        self.publisher_ = self.create_publisher(Point, "detection_node/localization", 10)
+        self.subscriber_ = self.create_subscription(DetectionArray, "detection_node/detection", self.callback, 10)
+        self.get_logger().info('Initializing localization publisher')
+
+    def callback(self, detectionArray: DetectionArray):
+        if detectionArray is None:
+            self.get_logger().info("No detection found.")
+            return
+        localization_array = LocalizationArray()
+
+        for detection in detectionArray:
+            localization = Localization()
+            x1, y1, x2, y2 = detection
+            bbox_xy = [x1, y1, x2, y2]
+            class_id = detection.hypothesis.class_id
+
+            angle = get_degree(self.img_size, bbox_xy)
+            pixel_distance = get_distance(pixel=self.img_size, bbox_xy=bbox_xy)
+            real_distance = convert_to_cm(pixel=pixel_distance)
+
+            localization.distance = real_distance
+            localization.angle = angle
+            localization.class_id = class_id
+
+            localization_array.localizations.append(localization)
+
+        self.publisher_.publish(msg=localization_array)
+        self.get_logger().info(f"Published ball localization")   
+        
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = Localization()
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
